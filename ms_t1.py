c283a80fbaf2d8cded4c096186000ada17b19482
luizhb <lhsbispo@yahoo.com.br>
2016-02-15 11:56:30 -0300

    tentei forcar o programa com a derivada que tinha feito pelo metodo de newton. pelo da bissecao nao consegui ver como iria colocar os valores para calcular

--- ms_t1.py
+++ ms_t1.py
@@ -1,9 +1,26 @@
 #! /usr/bin/env python
 
-import sys
+import math
 
-def main(argv):
-    return 0
+def funcao(x):
+    return math.pow(x,6)*math.exp(6.93/x)*math.exp(math.pow(a,2)*0.5*0.000145*(math.pow(x,2)-1/x))-4*a*math.exp(6.93)
 
-if __name__ == '__main__':
-    main(sys.argv)
+def derivadafuncao(x):
+    return math.pow(x,6)*math.exp(0.0000725*math.pow(a,2)*(math.pow(x,2)-1/x) + 6.93/x)*(0.0000725*math.pow(a,2)*(1/math.pow(x,2)+2*x) -6.93/math.pow(x,2)) + 6*math.pow(x,5)*math.exp(0.0000725*math.pow(a,2)*(math.pow(x,2)-1/x) + 6.93/x)
+
+a = int(0)
+
+x = float(input("Valor de x"))
+t = 0
+
+while(x != t and a <81):
+    t = x
+    x = x - funcao(x)/ derivadafuncao(x)
+
+    print ("x" + str(a) + " = " + str (x) +"\n")
+    a = a+1
+
+if (a==81):
+    print("\n\n Nao converge")
+else:
+    print("\n\n Valor de x: " + str(x))
